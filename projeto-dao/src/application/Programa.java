18bb34866c4f3d0e3a0bbaad5fd919e2cd0ed2be
Rafael Costa <raf.costa29@gmail.com>
2019-09-06 08:12:11 -0300

    Classe de Departamnetos

---
+++ projeto-dao/src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departamentos;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamentos obj = new Departamentos(1, "Books");
+		System.out.println(obj);
+	}
+
+}
